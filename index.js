b30255e5dd320558a9971e792595721fbc740ccf
Giuseppe Eletto <peppe.eletto@gmail.com>
2021-09-09 11:43:55 +0200

    feat: reduce code size
    * Single check of Boolean variables
    * Even smaller: -20 Bytes (~775 Bytes when minimized)

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+let Obj = Object
+
 /*
 Formats the given number using `Number#toLocaleString`.
 - If loc is a string, the value is expected to be a loc-key (for example: `de`).
@@ -7,7 +9,7 @@ Formats the given number using `Number#toLocaleString`.
 let toLocaleString = (num, loc, opts) => {
     if (typeof loc == 'string' || Array.isArray(loc))
         return num.toLocaleString(loc, opts)
-    if (loc == true || Object.keys(opts).length)
+    if (loc == true || Obj.keys(opts).length)
         return num.toLocaleString(undefined, opts)
     return num
 }
@@ -16,39 +18,45 @@ module.exports = (num, opts) => {
     if (!Number.isFinite(num))
         throw new TypeError(`Expected a finite num, got ${typeof num}: ${num}`)
 
-    opts = Object.assign({bits: false, binary: false}, opts)
+    opts = Obj.assign({bits: false, binary: false}, opts)
 
-    let UNITS = `${opts.bits ? 'b' : 'B'}${opts.binary ? 'K' : 'k'}MGTPEZY`
+	let bitByteUnit = opts.bits ? 'bit' : 'B'
 
     if (opts.signed && !num) // num == 0
-        return ` 0 ${UNITS[0]}`
+        return ` 0 ${bitByteUnit[0]}`
+
+	let binSiKilo = 'k', binPostfix = '', log = Math.log10, valUnit = 1000
+	if (opts.binary) {
+		binSiKilo = 'K'
+		binPostfix = 'i'
+		log = Math.log
+		valUnit += 24
+	}
+
+    let UNITS = `${bitByteUnit[0]}${binSiKilo}MGTPEZY`
 
     let prefix = num < 0 ? '-' : (opts.signed ? '+' : '')
     num = Math.abs(num)
 
-    let locOpts = Object.fromEntries(
-        Object.entries(opts).filter(([k, _]) => k.includes('ionDig'))
+    let locOpts = Obj.fromEntries(
+        Obj.entries(opts).filter(e => e[0].includes('ionDig'))
     )
 
     if (num < 1) {
-        let numStr = toLocaleString(num, opts.loc, locOpts)
+        let numStr = toLocaleString(num, opts.locale, locOpts)
         return `${prefix}${numStr} ${UNITS[0]}`
     }
 
-    let exp = Math.min(
-        Math.floor(opts.binary ? Math.log(num) / Math.log(1024) : Math.log10(num) / 3 /* log10(1000) */),
-        UNITS.length - 1)
-    num /= Math.pow(opts.binary ? 1024 : 1000, exp)
+    let exp = Math.min(Math.floor(log(num) / log(valUnit)), UNITS.length - 1)
+    num /= Math.pow(valUnit, exp)
 
-    if (!Object.keys(locOpts).length)
+    if (!Obj.keys(locOpts).length)
         num = num.toPrecision(3)
 
     let postfix = UNITS[exp]
-    if (exp) { // exp > 0
-        postfix += opts.binary ? 'i' : ''
-        postfix += opts.bits ? 'bit' : 'B'
-    }
+    if (exp) // exp > 0
+        postfix += binPostfix + bitByteUnit
 
-    let numStr = toLocaleString(Number(num), opts.loc, locOpts)
+    let numStr = toLocaleString(Number(num), opts.locale, locOpts)
     return `${prefix}${numStr} ${postfix}`
 }
